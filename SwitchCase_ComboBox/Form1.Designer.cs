af49320701fcee3a5b92becbe23074fcc07cb958
David Henrique <contatodavidhss@gmail.com>
2021-10-27 12:43:31 -0300

    Primeiro Commit

---
+++ SwitchCase_ComboBox/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace SwitchCase_ComboBox
+{
+    partial class frmPoligono
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPoligono));
+            this.cbxPoligono = new System.Windows.Forms.ComboBox();
+            this.lblEscolha = new System.Windows.Forms.Label();
+            this.lblLado = new System.Windows.Forms.Label();
+            this.txtLado = new System.Windows.Forms.TextBox();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblFormula = new System.Windows.Forms.Label();
+            this.txtFormula = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbxPoligono
+            // 
+            this.cbxPoligono.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxPoligono.FormattingEnabled = true;
+            this.cbxPoligono.Items.AddRange(new object[] {
+            "",
+            "Quadrado",
+            "Triângulo",
+            "Retângulo"});
+            this.cbxPoligono.Location = new System.Drawing.Point(165, 23);
+            this.cbxPoligono.Name = "cbxPoligono";
+            this.cbxPoligono.Size = new System.Drawing.Size(94, 21);
+            this.cbxPoligono.TabIndex = 0;
+            this.cbxPoligono.SelectedIndexChanged += new System.EventHandler(this.cbxPoligono_SelectedIndexChanged);
+            // 
+            // lblEscolha
+            // 
+            this.lblEscolha.AutoSize = true;
+            this.lblEscolha.Font = new System.Drawing.Font("Amazon Ember", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEscolha.Location = new System.Drawing.Point(22, 24);
+            this.lblEscolha.Name = "lblEscolha";
+            this.lblEscolha.Size = new System.Drawing.Size(137, 17);
+            this.lblEscolha.TabIndex = 1;
+            this.lblEscolha.Text = "Escolha um polígono:";
+            // 
+            // lblLado
+            // 
+            this.lblLado.AutoSize = true;
+            this.lblLado.Font = new System.Drawing.Font("Amazon Ember", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLado.Location = new System.Drawing.Point(82, 112);
+            this.lblLado.Name = "lblLado";
+            this.lblLado.Size = new System.Drawing.Size(36, 15);
+            this.lblLado.TabIndex = 2;
+            this.lblLado.Text = "Lado:";
+            // 
+            // txtLado
+            // 
+            this.txtLado.Location = new System.Drawing.Point(119, 109);
+            this.txtLado.Name = "txtLado";
+            this.txtLado.Size = new System.Drawing.Size(73, 20);
+            this.txtLado.TabIndex = 3;
+            this.txtLado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLado.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLado_KeyPress);
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Amazon Ember", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(75, 137);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(43, 15);
+            this.lblAltura.TabIndex = 4;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(119, 135);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(73, 20);
+            this.txtAltura.TabIndex = 5;
+            this.txtAltura.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAltura_KeyPress);
+            // 
+            // lblFormula
+            // 
+            this.lblFormula.AutoSize = true;
+            this.lblFormula.Font = new System.Drawing.Font("Amazon Ember", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFormula.Location = new System.Drawing.Point(107, 55);
+            this.lblFormula.Name = "lblFormula";
+            this.lblFormula.Size = new System.Drawing.Size(58, 15);
+            this.lblFormula.TabIndex = 6;
+            this.lblFormula.Text = "Fórmula";
+            // 
+            // txtFormula
+            // 
+            this.txtFormula.Enabled = false;
+            this.txtFormula.Location = new System.Drawing.Point(59, 73);
+            this.txtFormula.Name = "txtFormula";
+            this.txtFormula.Size = new System.Drawing.Size(156, 20);
+            this.txtFormula.TabIndex = 7;
+            this.txtFormula.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Amazon Ember", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(100, 176);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(69, 15);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Location = new System.Drawing.Point(59, 194);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(156, 20);
+            this.txtResultado.TabIndex = 9;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(59, 227);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(156, 33);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Font = new System.Drawing.Font("Amazon Ember", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.Location = new System.Drawing.Point(59, 266);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(75, 23);
+            this.btnApagar.TabIndex = 11;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click_1);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Amazon Ember", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(140, 266);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 12;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmPoligono
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(282, 307);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtFormula);
+            this.Controls.Add(this.lblFormula);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.txtLado);
+            this.Controls.Add(this.lblLado);
+            this.Controls.Add(this.lblEscolha);
+            this.Controls.Add(this.cbxPoligono);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmPoligono";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Área de um Polígono";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxPoligono;
+        private System.Windows.Forms.Label lblEscolha;
+        private System.Windows.Forms.Label lblLado;
+        private System.Windows.Forms.TextBox txtLado;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblFormula;
+        private System.Windows.Forms.TextBox txtFormula;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ SwitchCase_ComboBox/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchCase_ComboBox
+{
+    public partial class frmPoligono : Form
+    {
+        public frmPoligono()
+        {
+            InitializeComponent();
+        }
+
+        public void Limpar()
+        {
+            txtResultado.Clear();
+            txtLado.Clear();
+            txtAltura.Clear();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtLado_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) || char.IsSymbol(e.KeyChar) || char.IsWhiteSpace(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void txtAltura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) || char.IsSymbol(e.KeyChar) || char.IsWhiteSpace(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string tri = cbxPoligono.SelectedIndex.Equals(2).ToString();
+
+            if (cbxPoligono.SelectedIndex.Equals(-1) || cbxPoligono.SelectedIndex.Equals(0))
+            {
+                MessageBox.Show("Insira um polígono!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtResultado.Clear();
+                txtFormula.Clear();
+                cbxPoligono.Focus();
+            }
+            else if (txtLado.Text == "")
+            {
+                MessageBox.Show("Insira o valor do Lado!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtResultado.Clear();
+                txtLado.Focus();
+            }
+            else if (txtAltura.Text == "" && cbxPoligono.SelectedIndex.Equals(3))
+            {
+                MessageBox.Show("Insira o valor da Altura!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtResultado.Clear();
+                txtAltura.Focus();
+            }
+            else if (txtAltura.Text == "" && cbxPoligono.SelectedIndex.Equals(2))
+            {
+                MessageBox.Show("Insira o valor da Altura!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtResultado.Clear();
+                txtAltura.Focus();
+            }
+            else if (double.Parse(txtLado.Text) <= 0)
+            {
+                MessageBox.Show("Insira um valor maior que 0", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtResultado.Clear();
+                txtLado.Focus();
+            }
+            else
+            {
+                switch (cbxPoligono.SelectedItem)
+                {
+                    case "Quadrado":
+                        txtResultado.Text = (txtLado.Text + " x " + txtLado.Text + " = " + (Convert.ToDouble(txtLado.Text) * Convert.ToDouble(txtLado.Text)).ToString());
+                        break;
+
+                    case "Triângulo":
+                        txtResultado.Text = (txtLado.Text + " x " + txtAltura.Text + " / 2 = " + (Convert.ToDouble(txtLado.Text) * Convert.ToDouble(txtAltura.Text) / 2).ToString());
+                        break;
+
+                    case "Retângulo":
+                        txtResultado.Text = (txtLado.Text + " x " + txtAltura.Text + " = " + (Convert.ToDouble(txtLado.Text) * Convert.ToDouble(txtAltura.Text)).ToString());
+                        break;
+                }
+            }
+        }
+
+        private void cbxPoligono_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbxPoligono.SelectedItem)
+            {
+                case "Quadrado":
+                    txtFormula.Text = "Lado x Lado";
+                    txtAltura.ReadOnly = true;
+                    Limpar();
+                    txtLado.Focus();
+                    break;
+
+                case "Triângulo":
+                    txtFormula.Text = "Lado x Altura / 2";
+                    txtAltura.ReadOnly = false;
+                    Limpar();
+                    txtLado.Focus();
+                    break;
+
+                case "Retângulo":
+                    txtFormula.Text = "Lado x Altura";
+                    txtAltura.ReadOnly = false;
+                    Limpar();
+                    txtLado.Focus();
+                    break;
+
+                case "":
+                    Limpar();
+                    txtFormula.Clear();
+                    break;
+            }
+        }
+
+        private void btnApagar_Click_1(object sender, EventArgs e)
+        {
+            Limpar();
+            txtFormula.Clear();
+            cbxPoligono.SelectedItem = "";
+        }
+    }
+}
